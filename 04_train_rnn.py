05524a42409e548557e14686728b32dd82708d29
goodluckoguzie <goodluckoguzie1@gmail.com>
2022-08-10 21:55:51 +0100

    updated all the scripts with steps to follow .

---
+++ 04_train_rnn.py
@@ -0,0 +1,265 @@
+import numpy 
+import torch
+import torch.nn as nn
+from RNN.RNN import RNN,Rnn
+import time
+import os, time, datetime
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+import argparse
+parser = argparse.ArgumentParser("mode asigning")
+parser.add_argument('--epochs', type=int, help="epochs")
+parser.add_argument('--mode', type=str, required=True,help="normal,window,reward")
+args = parser.parse_args()
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+latents = 31
+actions = 2
+hiddens = 256
+epochs = args.epochs
+sub_epochs = 5
+train_window = 10 
+batch_size = 64
+timestep = 50
+
+dataset = torch.load('./data/saved_rollout_rnn_train.pt')# our training dataset got from extract_data_for_rnn.py . note that the time step here and there must tally 
+
+
+def trains(mode='normal'):
+    
+    if mode == 'normal':
+        class MDN_Dataset(torch.utils.data.Dataset):
+            def __init__(self, MDN_data):
+                self.MDN_data = MDN_data
+            def __len__(self):
+                return len(self.MDN_data)
+
+            def __getitem__(self, idx):
+                data = self.MDN_data[idx]
+                obs = data['obs_sequence']
+                action = data['action_sequence']
+                #reward = data['reward_sequence']
+                return (action, obs)
+
+        train_dataset = MDN_Dataset(dataset)
+        train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+
+        l1 = nn.L1Loss()
+        rnn = RNN(latents, actions, hiddens).to(device)
+        optimizer = torch.optim.Adam(rnn.parameters(), lr=1e-4)
+        start=time.time()
+        best_loss = float("inf")
+        epoch_ = []
+        epoch_train_loss = []
+
+        rnn.train()
+        for epoch in range(1, epochs + 1):
+            train_loss = 0
+            for batch_idx, (action, obs) in enumerate(train_dataloader):# get a batch of timesteps seperated by episodes
+                # we have 200 timesteps in an episode . 
+                current_timestep = obs[:, :-1, :].to("cuda:0")  # remove the last timestep . input now has 199 time steps 
+                action = action[:, :-1, :]          # remove the action taken in the last timestep . input now has 199 actions time steps 
+                nxt_timestep = obs[:, 1:, :].to("cuda:0")  #remove the first timestep .the label contains 199 timesteps because we excluded the the fist time step
+                states = torch.cat([current_timestep, action], dim=-1)
+                predicted_nxt_timestep, _, _ = rnn(states)
+
+                loss_rnn = l1(predicted_nxt_timestep, nxt_timestep)
+                optimizer.zero_grad()
+                loss_rnn.backward()
+                train_loss += loss_rnn.item()
+                optimizer.step()
+            train_loss = train_loss/ len(train_dataset)
+
+
+            if train_loss <= best_loss:
+                if not os.path.exists('MODEL'):
+                    os.makedirs('MODEL')
+            
+                torch.save(rnn.state_dict(), './MODEL/MDN_RNN_normal.pt')
+                best_loss = train_loss
+        
+            epoch_.append(epoch)
+            epoch_train_loss.append(train_loss)
+
+            print('EPOCH : {} Average_loss : {}'.format(epoch, train_loss))   
+
+
+
+    elif mode == "window":
+
+        def create_inout_sequences(input_data,action_data, tw):
+            inout_seq = []
+            for i in range(timestep-tw):#the timestep is gotten from the extracted data this must tally with that of extract_data_for_rnn.py
+                train_seq = input_data[:,i:i+tw,:]
+                train_label = input_data[:,i+tw:i+tw+1,:]
+
+                action_seq = action_data[:,i:i+tw,:]
+                action_label = action_data[:,i+tw:i+tw+1,:]
+                inout_seq.append((train_seq ,train_label,action_seq,action_label))
+            return inout_seq
+
+        class MDN_Dataset(torch.utils.data.Dataset):
+            def __init__(self, MDN_data):
+                self.MDN_data = MDN_data
+            def __len__(self):
+                return len(self.MDN_data)
+
+            def __getitem__(self, idx):
+                data = self.MDN_data[idx]
+                obs = data['obs_sequence']
+                action = data['action_sequence']
+                #reward = data['reward_sequence']
+                return (action, obs)
+
+        train_dataset = MDN_Dataset(dataset)
+        train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+
+        l1 = nn.L1Loss()
+        rnn = RNN(latents, actions, hiddens).to(device)
+        optimizer = torch.optim.Adam(rnn.parameters(), lr=1e-4)
+
+        best_loss = float("inf")
+        epoch_ = []
+        epoch_train_loss = []
+
+        rnn.train()
+        for epoch in range(1, epochs + 1):
+            train_loss = 0
+        
+            for batch_idx, (action, obs) in enumerate(train_dataloader):# get a batch of timesteps seperated by episodes
+                print("batch_idx")
+                print(batch_idx)
+
+                train_inout_seq = create_inout_sequences(obs, action, train_window) #using the a sliding window of 10 . the the first 10 time step and the 11th timetep will be our label.
+                                                                                    # next shift the sliding window a step ahead now our label is the 12th timestep
+                for i in range(sub_epochs):# we train our model per window slides
+
+                    w = 0
+                    for current_timestep, nxt_timestep,action,_ in train_inout_seq:
+        
+                        action = action.to("cuda:0")  
+                        current_timestep = current_timestep.to("cuda:0")  
+                        nxt_timestep = nxt_timestep.to("cuda:0")  
+                        states = torch.cat([current_timestep, action], dim=-1)
+                        optimizer.zero_grad()
+                        predicted_nxt_timestep, _, _ = rnn(states)
+                        predicted_nxt_timestep = predicted_nxt_timestep[:, -1:, :] #get the last array for the predicted class
+
+                        loss_rnn = l1(predicted_nxt_timestep, nxt_timestep)
+                        optimizer.zero_grad()
+                        loss_rnn.backward()
+                        train_loss += loss_rnn.item()
+                        optimizer.step()
+                        w = w+1
+                    
+                    train_loss = train_loss/ len(train_dataloader)
+
+                    if train_loss <= best_loss:
+                        if not os.path.exists('model'):
+                            os.makedirs('model')
+                        torch.save(rnn.state_dict(), './MODEL/MDN_RNN_window.pt')
+                        best_loss = train_loss
+                
+                    epoch_.append(sub_epochs)
+                    epoch_train_loss.append(train_loss)
+
+                    print('sub_epochs : {} Average_loss : {}'.format(i, train_loss)) 
+                    print('batch_idx : {} '.format(batch_idx))
+                    print('MAIN EPOCH : {} '.format(epoch)) 
+
+
+    elif mode == "reward":
+
+        def create_inout_sequences(input_data,action_data, reward_data,tw):
+            inout_seq = []
+            for i in range(timestep-tw):#the timestep is gotten from the extracted data this must tally with that of extract_data_for_rnn.py
+                train_seq = input_data[:,i:i+tw,:]
+                train_label = input_data[:,i+tw:i+tw+1,:]
+
+                action_seq = action_data[:,i:i+tw,:]
+                action_label = action_data[:,i+tw:i+tw+1,:]
+
+                reward_seq = reward_data[:,i:i+tw,:]
+                reward_label = reward_data[:,i+tw:i+tw+1,:]
+                inout_seq.append((train_seq ,train_label,action_seq,action_label,reward_seq,reward_label))
+            return inout_seq
+
+        class MDN_Dataset(torch.utils.data.Dataset):
+            def __init__(self, MDN_data):
+                self.MDN_data = MDN_data
+            def __len__(self):
+                return len(self.MDN_data)
+
+            def __getitem__(self, idx):
+                data = self.MDN_data[idx]
+                obs = data['obs_sequence']
+                action = data['action_sequence']
+                reward = data['reward_sequence']
+                return (action, obs,reward)
+
+        train_dataset = MDN_Dataset(dataset)
+        train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+
+        l1 = nn.L1Loss()
+        reward = 1
+        rnn = Rnn(latents, actions,reward, hiddens).to(device)
+        optimizer = torch.optim.Adam(rnn.parameters(), lr=1e-4)
+        rewrd = 1
+        best_loss = float("inf")
+        epoch_ = []
+        epoch_train_loss = []
+        rnn.train()
+        for epoch in range(1, epochs + 1):
+            train_loss = 0
+        
+            for batch_idx, (action, obs,reward) in enumerate(train_dataloader):# get a batch of timesteps seperated by episodes
+                print("batch_idx")
+                print(batch_idx)
+
+                train_inout_seq = create_inout_sequences(obs, action,reward, train_window) #using the a sliding window of 10 . the the first 10 time step and the 11th timetep will be our label.
+                                                                        # next shift the sliding window a step ahead now our label is the 12th timestep
+
+                for i in range(sub_epochs):# we train our model per window slides
+                    w = 0
+                    for current_timestep, nxt_timestep,action,_ ,reward, nxt_reward in train_inout_seq:
+                        #print(reward)
+                        #print(input.shape)
+                        action = action.to("cuda:0")  
+                        current_timestep = current_timestep.to("cuda:0")  
+                        nxt_timestep = nxt_timestep.to("cuda:0")
+                        reward = reward.type(torch.cuda.FloatTensor)
+                        #print(reward)
+                        states = torch.cat([current_timestep, action,reward], dim=-1)
+                        optimizer.zero_grad()
+    
+                        predicted_nxt_timestep, _, _ = rnn(states)
+                        predicted_nxt_timestep = predicted_nxt_timestep[:, -1:, :] #get the last array for the predicted class
+
+                        loss_rnn = l1(predicted_nxt_timestep, nxt_timestep)
+                        optimizer.zero_grad()
+                        loss_rnn.backward()
+                        train_loss += loss_rnn.item()
+                        optimizer.step()
+                        w = w+1
+                    
+                    train_loss = train_loss/ len(train_dataloader)
+
+                    if train_loss <= best_loss:
+                        if not os.path.exists('model'):
+                            os.makedirs('model')
+                        torch.save(rnn.state_dict(), './MODEL/MDN_RNN_reward.pt')
+                        best_loss = train_loss
+                
+                    epoch_.append(sub_epochs)
+                    epoch_train_loss.append(train_loss)
+
+                    print('sub_epochs : {} Average_loss : {}'.format(i, train_loss)) 
+                    print('batch_idx : {} '.format(batch_idx))
+            
+
+                    print('MAIN EPOCH : {} '.format(epoch)) 
+
+
+     
+trains(args.mode)
--- README.md
+++ README.md
@@ -1,1 +1,84 @@
 # World-Models
+
+# World Models Implementation In Chainer
+This is a  complete implementation, in Socnavenv , of the World Models framework described by David Ha and Jürgen Schmidhuber: https://arxiv.org/abs/1803.10122
+
+
+
+## World Models Summary
+
+Here is a quick summary of Ha & Schmidhuber's World Models framework. The framework aims to train an agent that can perform well in virtual gaming environments. Ha & Schmidhuber's experiments were done in the [CarRacing-v0](https://gym.openai.com/envs/CarRacing-v0/) (from [OpenAI gym](https://gym.openai.com/)), and [ViZDoom: Take Cover](https://github.com/mwydmuch/ViZDoom/tree/master/scenarios#take-cover) environments.In our case we are reproducing this in [socnavenv](https://github.com/robocomp/gsoc22-socnavenv) environment.
+
+
+World Models consists of three main components: Vision (**V**), Model (**M**), and Controller (**C**) that interact together to form an agent:
+
+
+ 
+**V** consists of a convolutional [Variational Autoencoder (VAE)](https://arxiv.org/abs/1606.05908), which compresses frames taken from the gameplay into a latent vector *z*. **M** consists of a [Long Short-Term Memory Recurrent Neural Networks](https://arxiv.org/pdf/1909.09586.pdf), from a [Recurrent Neural Network (RNN)](https://en.wikipedia.org/wiki/Recurrent_neural_network). This RNN takes latent vectors *z* from **V** and predicts the next frame. And finally **C** is a simple single layer linear model that maps the output from **M** to actions to perform in the environment. **C** is trained using [Evolution Strategies](https://blog.openai.com/evolution-strategies/), particularly the [CMA-ES](https://arxiv.org/abs/1604.00772) algorithm.
+
+
+
+### Setup
+
+* `conda install numpy chainer scipy Pillow imageio numba cupy`  *(cupy if using GPU)*  
+* `pip install gym`
+
+
+
+
+## Training
+
+Training and testing our world model consists in 7 steps: 1) Generating the train and test data for VAE and RNN, 2)Generating the train and test data for RNN, 3) Training the VAE to learn a compressed representation of that data, 4) Training the RNN on sequences of the RNN train data, and 4) Testing the Trained VAE 5) Testing the Trained RNN, 6) Training the Controller, and 7) Testing the Controller.
+
+### 1. Random Rollouts
+1) Generating VAE data. Data in this case is sequences of images from the environment. To gather images from 2000 rollouts, run the command 
+
+```sh
+python3 01_generate_vae_data.py  --rollouts 2000 --testrollouts 100
+```
+### 2. Random Rollouts
+2) Generating RNN data. Data in this case is sequences of images from the environment. To gather images from 2000 episodes, run the command 
+
+```sh
+python3 02_generate_rnn_data.py  --episodes 2000 --testepisodes 100
+```
+### 3. Vision (V)
+3) Training the VAE. To train on our data, you can run the command
+
+```sh
+python3 03_train_vae.py --epochs 1000 --max_samples 2000
+```
+### 3. Model (M)
+4) Training the RNN. To train on our data, you can run the command.The mode can either be window, reward, dream or normal
+
+```sh
+python3 04_train_rnn.py --epochs 1000 --mode window
+```
+
+
+5) Test our VAE. To test on our data, you can run the command.
+
+```sh
+python3 05_draw_vae_result.py 
+```
+
+
+6) Test our RNN. To test on our data, you can run the command.The mode can either be window, reward, dream or normal
+
+```sh
+python3 06_draw_rnn_result.py --mode window
+```
+
+### 7. Controller (C)
+
+3) Training the Controller.You can run the command
+
+```sh
+python3 07_train_controller.py --epochs 100
+```
+
+
+
+## License
+
+The original research for World Models was conducted by Ha & Schmidhuber, but this code is entirely written by myself (Adeel Mufti). I am releasing it under the [MIT](https://opensource.org/licenses/MIT) license.
