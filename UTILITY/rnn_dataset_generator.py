6c7f2ab2513b30682ecdac7db3d4c1a3781af952
goodluckoguzie <goodluckoguzie1@gmail.com>
2022-08-21 21:18:07 +0100

    rnn generator

---
+++ UTILITY/rnn_dataset_generator.py
@@ -0,0 +1,65 @@
+import sys
+import torch
+import torchvision
+import torch.optim as optim
+import argparse
+import torch.nn as nn
+from torch.utils.data import DataLoader
+import numpy as np
+import os, time, datetime
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+time_steps = 200
+
+
+train_dataset = torch.load('./data/saved_vae_rollout_train.pt')
+val_dataset = torch.load('./data/saved_vae_rollout_validation.pt')
+
+
+
+def pad_tensor( tensor, pad):
+    pad_size = pad - tensor.size(0)
+    return torch.cat([tensor.to(device), torch.zeros([pad_size, tensor.size(1)]).to(device)], dim=0)
+
+print(len(train_dataset))
+
+def fit_dataset_to_rnn(dataset):
+    
+    for episode_idx, episode_data in enumerate(dataset.values()):
+        print(episode_idx)
+
+
+        obs_sequence = episode_data['obs_sequence']
+        # print(obs_sequence.shape)
+
+        action_sequence = episode_data['action_sequence']
+        reward_sequence = episode_data['reward_sequence']
+        done_sequence = episode_data['done_sequence']
+
+        obs_sequence = torch.stack(obs_sequence, dim=0).squeeze(1)
+
+        obs_sequence = pad_tensor(obs_sequence, pad=time_steps).cpu().detach().numpy()
+        # obs_sequence = utility.normalised(obs_sequence) #normilised our dataset 
+        obs_sequence = torch.from_numpy(obs_sequence) 
+
+        
+        done = [int(d) for d in done_sequence]
+        done = torch.tensor(done).unsqueeze(-1)
+        done_sequence = pad_tensor(done, pad=time_steps)
+        
+        action_sequence = torch.stack(action_sequence, dim=0).squeeze(1)
+        action_sequence = pad_tensor(action_sequence, pad=time_steps)
+
+        
+        reward = torch.tensor(reward_sequence).unsqueeze(-1)
+        reward_sequence = pad_tensor(reward, pad=time_steps)
+        reward = pad_tensor(reward, pad=time_steps)
+        
+        episode_data['obs_sequence'] = obs_sequence
+        episode_data['action_sequence'] = action_sequence
+        episode_data['done_sequence'] = done_sequence
+        episode_data['reward_sequence'] = reward_sequence
+    return episode_data
+
+
+
