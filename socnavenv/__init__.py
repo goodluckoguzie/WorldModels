aade42c32e3865136baa90f2c9df484f51993b70
goodluckoguzie <goodluckoguzie1@gmail.com>
2022-09-14 20:13:04 +0100

    Simplified version

---
+++ socnavenv/__init__.py
@@ -0,0 +1,13 @@
+from gym.envs.registration import register
+
+register(
+    id='SocNavEnv-v0',
+    entry_point='socnavenv.envs:SocNavEnv',
+)
+
+register(
+    id='SocNavEnv-v1',
+    entry_point='socnavenv.envs:SocNavEnv_v1',
+)
+
+
