b6a30f78797aa3cd511dfb87192eaf8e394d79f9
goodluckoguzie <goodluckoguzie1@gmail.com>
2022-09-24 23:59:05 +0100

    updated

--- agents/duelingDQN.py
+++ agents/duelingDQN.py
@@ -72,9 +72,6 @@ class DuelingDQNAgent:
         # declaring the network
         self.duelingDQN = DuelingDQN(self.input_layer_size, self.hidden_layers, self.v_net_layers, self.a_net_layers).to(self.device)
         
-        # initializing using xavier initialization
-        self.duelingDQN.apply(self.xavier_init_weights)
-
         #initializing the fixed targets
         self.fixed_targets = DuelingDQN(self.input_layer_size, self.hidden_layers, self.v_net_layers, self.a_net_layers).to(self.device)
         self.fixed_targets.load_state_dict(self.duelingDQN.state_dict())
@@ -202,11 +199,11 @@ class DuelingDQNAgent:
         elif action == 5:
             return np.array([-1, 0], dtype=np.float32)
         
-        # elif action == 6:
-        #     return np.array([-0.8, +0.4], dtype=np.float32)
+        elif action == 6:
+            return np.array([-0.8, +0.4], dtype=np.float32)
 
-        # elif action == 7:
-        #     return np.array([-0.8, -0.4], dtype=np.float32)
+        elif action == 7:
+            return np.array([-0.8, -0.4], dtype=np.float32)
         
         else:
             raise NotImplementedError
@@ -223,7 +220,7 @@ class DuelingDQNAgent:
         
         else:
             # explore
-            act = np.random.randint(0, 6)
+            act = np.random.randint(0, 8)
             return self.discrete_to_continuous_action(act), act 
     
     def calculate_grad_norm(self):
@@ -344,7 +341,7 @@ class DuelingDQNAgent:
 
                 # preprocessing the observation, i.e padding the observation with zeros if it is lesser than the maximum size
                 next_obs = self.preprocess_observation(next_obs)
-
+                
                 # rendering if reqd
                 if self.render and ((i+1) % self.render_freq == 0):
                     self.env.render()
