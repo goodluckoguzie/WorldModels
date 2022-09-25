4dbf5976d1b917eb57d968c45850d2b89a37cf24
goodluckoguzie <goodluckoguzie1@gmail.com>
2022-09-25 06:58:51 +0100

    updated

--- 01_generate_vae_dataset.py
+++ 01_generate_vae_dataset.py
@@ -29,36 +29,36 @@ if not os.path.exists(rollout_dir):
 total_episodes = args.episodes
 
 
-    def discrete_to_continuous_action(self, action:int):
-        """
-        Function to return a continuous space action for a given discrete action
-        """
-        if action == 0:
-            return np.array([0, 0.25], dtype=np.float32) 
-        
-        elif action == 1:
-            return np.array([0, -0.25], dtype=np.float32) 
+def discrete_to_continuous_action(self, action:int):
+    """
+    Function to return a continuous space action for a given discrete action
+    """
+    if action == 0:
+        return np.array([0, 0.25], dtype=np.float32) 
+    
+    elif action == 1:
+        return np.array([0, -0.25], dtype=np.float32) 
 
-        elif action == 2:
-            return np.array([1, 0.125], dtype=np.float32) 
-        
-        elif action == 3:
-            return np.array([1, -0.125], dtype=np.float32) 
+    elif action == 2:
+        return np.array([1, 0.125], dtype=np.float32) 
+    
+    elif action == 3:
+        return np.array([1, -0.125], dtype=np.float32) 
 
-        elif action == 4:
-            return np.array([1, 0], dtype=np.float32)
+    elif action == 4:
+        return np.array([1, 0], dtype=np.float32)
 
-        elif action == 5:
-            return np.array([-1, 0], dtype=np.float32)
-        
-        elif action == 6:
-            return np.array([-0.8, +0.4], dtype=np.float32)
+    elif action == 5:
+        return np.array([-1, 0], dtype=np.float32)
+    
+    elif action == 6:
+        return np.array([-0.8, +0.4], dtype=np.float32)
 
-        elif action == 7:
-            return np.array([-0.8, -0.4], dtype=np.float32)
-        
-        else:
-            raise NotImplementedError
+    elif action == 7:
+        return np.array([-0.8, -0.4], dtype=np.float32)
+    
+    else:
+        raise NotImplementedError
 
 def preprocess_observation(obs):
     """
