e9b99a3721c7d02593e4cc4dcd1b9392335bdaba
goodluckoguzie <goodluckoguzie1@gmail.com>
2022-12-12 21:11:42 +0000

    x

--- 05_train_rnn_main_worldframe.py
+++ 05_train_rnn_main_worldframe.py
@@ -133,7 +133,7 @@ class RNN_MODEL():
         self.run_name = None
         # self.seq_len = None
         self.run_name = None
-        self.window = Seq_len.seq_1
+        self.window = Seq_len.seq_4
 
                 # setting values from config file
         self.configure(self.config)
@@ -149,7 +149,7 @@ class RNN_MODEL():
         self.ckpt_dir = data.ckpt_dir#'ckpt'
         self.rnnsave = data.rnnsave#'ckpt'
         self.data_path = data.data_dir 
-        self.seq_len = Seq_len.seq_len_1
+        self.seq_len = Seq_len.seq_len_4
         episode_length = data.time_steps
 
         print(self.seq_len) 
