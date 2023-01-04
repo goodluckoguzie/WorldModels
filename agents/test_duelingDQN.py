f2f4419ebf4532dfcb0c8366899b78351e8d3a5a
goodluckoguzie <goodluckoguzie1@gmail.com>
2023-01-04 08:31:54 +0000

    updated

--- agents/test_duelingDQN.py
+++ agents/test_duelingDQN.py
@@ -438,7 +438,7 @@ class DuelingDQNAgent:
 
 if __name__ == "__main__":
     env = gym.make("SocNavEnv-v1")
-    env.configure("./configs/env_timestep_1.yaml")
+    env.configure("./configs/env_timestep_0_5.yaml")
     env.set_padded_observations(True)
 
     # config file for the model
