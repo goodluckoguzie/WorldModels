8bbf5aecdc98a7fbed48fb6eda2000f0d0146d17
goodluckoguzie <goodluckoguzie1@gmail.com>
2023-01-20 11:28:41 +0000

    x

--- 05_train_rnn_main_Robotframe.py
+++ 05_train_rnn_main_Robotframe.py
@@ -148,7 +148,8 @@ class RNN_MODEL():
         # self.seq_len = None
         self.run_name = None
         # self.window = Seq_len.seq_16
-        self.window = Seq_len.seq_8
+        # self.window = Seq_len.seq_8
+        self.window = Seq_len.seq_4
 
                 # setting values from config file
         self.configure(self.config)
@@ -166,7 +167,8 @@ class RNN_MODEL():
         self.rnnsave = data.rnnsave#'ckpt'
         self.data_path = data.data_dir 
         # self.seq_len = Seq_len.seq_len_16
-        self.seq_len = Seq_len.seq_len_8
+        # self.seq_len = Seq_len.seq_len_8
+        self.seq_len = Seq_len.seq_len_4
         episode_length = data.time_steps
 
         print(self.seq_len) 
